e3bc930526c06b27cb13d69ac5b20507f2c69a11
hahwul <hahwul@gmail.com>
2016-05-31 14:36:43 +0900

    v1.0

--- src/generate_report.rb
+++ src/generate_report.rb
@@ -17,10 +17,25 @@ end
 
 def generate_report(app_pointer)
  report_html=""
+ workspace = app_pointer.getworkspace
  report=[["App.Package",app_pointer.getpackage],
 ["App.File_path",app_pointer.getfile],
 ["App.Main",app_pointer.getmain],
-["App.Permission",app_pointer.getperm.gsub("\r\n", '<br>')],
+["App.Permission",app_pointer.getperm.gsub("android.", '<br>android.')],
+["App.Decompile","<table>
+<tr>
+ <td>UNZIP: Analysis of native code, resources , etc.</td><td><a href='file:#{workspace}/1_unzip/' target='_blank' style='color: red'>go link</a></td>
+</tr>
+<tr>
+ <td>SMALI CODE: View source code viewer</td><td><a href='file:#{workspace}/2_apktool/' target='_blank' style='color: red'>go link</a></td>
+</tr>
+<tr>
+ <td>JAR CLASS: View jd-gui</td><td><a href='file:#{workspace}/3_dex2jar/' target='_blank' style='color: red'>go link</a></td>
+</tr>
+<tr>
+ <td>JAVA CODE: View source code viewer</td><td><a href='file:#{workspace}/4_jad/' target='_blank' style='color: red'>go link</a></td>
+</tr>
+</table>"],
 ["App.Str_point",str_point(app_pointer)]
 ]
  table=Table.new(report)
@@ -28,7 +43,7 @@ def generate_report(app_pointer)
  
  f = File.open(File.dirname(__FILE__)+"/../template/report_template.html","r")
  report_html = f.read()
- report_html.insert(report_html.index("<!-- {report} -->")+30,table_html)   # ^ is checking string 
+ report_html.insert(report_html.index("<!-- {report} -->")+24,table_html)   # ^ is checking string 
  puts report_html
  wf = File.open(app_pointer.getdirectory+".html","w")
  wf.puts report_html
