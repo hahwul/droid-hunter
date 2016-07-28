4100dd384bc41ed52910e540ecdea2a8891d6bbb
hahwul <hahwul@gmail.com>
2016-07-28 11:28:52 +0900

    v2.0

---
+++ install.sh
@@ -0,0 +1,11 @@
+#/usr/bin/bash
+echo 'Install droid-hunter'
+echo ' -> install gem'
+echo ' -> gem::html-table'
+gem install html-table
+echo ' -> gem::colorize'
+gem install colorize
+echo ' -> set command'
+MYPWD=`pwd`
+echo '#/usr/bin/ruby\nruby '$MYPWD'dhunter.rb' >> /usr/bin/dhunter
+echo 'Finish. run a dhunter'
