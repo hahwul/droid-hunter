347c7cb80297ee36a50373c5e177508b3d607beb
hahwul <hahwul@gmail.com>
2016-08-12 18:28:51 +0900

    v2.0

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ echo ' -> gem::colorize'
 gem install colorize
 echo ' -> set command'
 MYPWD=`pwd`
-echo '#/usr/bin/ruby\nruby '$MYPWD'dhunter.rb' >> /usr/bin/dhunter
+echo '#/usr/bin/ruby
+ruby '$MYPWD'/dhunter.rb $*' >> /usr/bin/dhunter
 echo 'Finish. run a dhunter'
